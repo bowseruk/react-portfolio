2de979daa393f8ec55ab7f29ec4c911dd4953484
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-03-15 21:53:31 +0000

    Added the router element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import Layout from './components/layout';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/home.js'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    // loader: rootLoader,
+    // children: [
+    //   {
+    //     path: "events/:id",
+    //     element: <Event />,
+    //     loader: eventLoader,
+    //   },
+    // ],
+  },
+], {
+  basename: "/react-portfolio/",
+});
 
 function App() {
   return (
     <div className="App">
       <Layout>
-
+        <RouterProvider router={router} />
       </Layout>
     </div>
   );
