20a11ccd8dd9e9698868c4733d71722c8fbf5647
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-03-20 16:51:13 +0000

    Added placeholder for project without screenshot

--- src/components/pages/project.js
+++ src/components/pages/project.js
@@ -6,10 +6,11 @@ import projectsData from '../../projects.json'
 
 function Project(props) {
     const {projectID} = useParams()
+    const image = (image) => (image === "") ? "https://placekitten.com/400/300" : image;
     console.log(projectID)
         return (
            <Layout>
-            <RetroDex id={projectsData[projectID].id + 1} skills={projectsData[projectID].skills} name={projectsData[projectID].name} type="Project" description={projectsData[projectID].description} github={projectsData[projectID].github} image={projectsData[projectID].image} sites={[["Live Site", projectsData[projectID].site]]} />
+            <RetroDex id={projectsData[projectID].id + 1} skills={projectsData[projectID].skills} name={projectsData[projectID].name} type="Project" description={projectsData[projectID].description} github={projectsData[projectID].github} image={image(projectsData[projectID].image)} sites={[["Live Site", projectsData[projectID].site]]} />
         </Layout> 
         ) 
     } 
