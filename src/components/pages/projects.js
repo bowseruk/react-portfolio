49435f2fd2f91bd0d610f4a2f1d68d9476f3db6f
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-03-20 15:48:18 +0000

    fixed switching project

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import getChar from '../characters/char-images.js'
 import Layout from '../layout.js'
 import BattleScene from "../battleScene.js";
