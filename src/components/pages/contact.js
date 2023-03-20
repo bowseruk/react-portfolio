2368af1c7696d3f91ffdd913c7a1f864697d8fb0
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-03-20 17:23:56 +0000

    Fixed contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import Layout from "../layout";
+import './contact.css'
 
-class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            message: ""
-        };
-    }
-    render() {
-        return <Layout>
-            <div>
-                <div>
+function Contact(props) {
+    const navigate = useNavigate()
+    return (
+        <Layout>
+            <div className="retrodex">
+                <div className="retrodex-screen">
                     <h2>Contact</h2>
+                    <p>Please use the form if you have an enquiry.</p>
                     <form>
-                    <input />
-                    <input />
-                    <textarea />
-                    <button >Submit</button>
+                        <div><label htmlFor="name">Name: </label><input id="name" name="name" /></div>
+                        <div><label htmlFor="email">Email: </label><input id="email" name="email" type="email" /></div>
+                        <div><label htmlFor="enquiry">Enquiry: </label><textarea id="enquiry"  name="enquiry"/></div>
+                        <div><button type="button" onClick={() => navigate("/")}>Submit</button></div>
                     </form>
                 </div>
             </div>
         </Layout>
-    }       
+    )
 }
 
 export default Contact
